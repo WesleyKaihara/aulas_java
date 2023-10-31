1bf4b23fe47633063d5fbba3e0a667aa809cc07c
wesleykaihara <wesleykaihara@gmail.com>
2023-10-30 21:23:25 -0300

    Add Setters

--- aula01/src/main/java/com/wesley/demo/DemoApplication.java
+++ aula01/src/main/java/com/wesley/demo/DemoApplication.java
@@ -12,8 +12,18 @@ public class DemoApplication {
 
 		var produto = new Produto(123, "Suco", 12.50);
 		System.out.println("ID: " + produto.getId() +
-						   "\nNOME PRODUTO: " + produto.getNome() +
+						   "\nDESCRIÇÃO: " + produto.getDescricao() +
 				           "\nVALOR: R$ " + produto.getValor());
+
+		produto.setDescricao("Suco de Laranja");
+		produto.setValor(15.99);
+
+		System.out.println("---------------------------");
+
+		System.out.println("ID: " + produto.getId() +
+				"\nDESCRIÇÃO: " + produto.getDescricao() +
+				"\nVALOR: R$ " + produto.getValor());
+
 	}
 
 }
--- aula01/src/main/java/models/Produto.java
+++ aula01/src/main/java/models/Produto.java
@@ -2,13 +2,13 @@ package models;
 
 public class Produto {
     private Integer id;
-    private String nome;
+    private String descricao;
     private Double valor;
 
     public Produto () {}
-    public Produto (Integer id, String nome, Double valor) {
+    public Produto (Integer id, String descricao, Double valor) {
         this.id = id;
-        this.nome = nome;
+        this.descricao = descricao;
         this.valor = valor;
     }
 
@@ -16,12 +16,24 @@ public class Produto {
         return this.id;
     }
 
-    public String getNome() {
-        return this.nome;
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
     }
 
     public Double getValor() {
         return this.valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
     }
 
 }
